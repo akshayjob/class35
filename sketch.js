367141e4129abbd393d56b5d0218c4866a381487
akshayjob <86850161+akshayjob@users.noreply.github.com>
2021-07-23 20:40:44 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+var gameState = 0;
+var playerCount;
+var form, player, game;
+
+
+function setup(){
+  database = firebase.database();
+  //console.log(database);
+  createCanvas(500,500);
+
+ game = new Game();
+ game.gameState();
+ game.start();
+}
+
+function draw(){
+  background("white");
+  
+    
+    drawSprites();
+  
+}
+
